22708aa86112031a26437123dc8565049ddf160a
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-08 01:22:12 +0200

    updated sass extension

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { useEffect } from "react";
-import "./index.css";
+import "./index.scss";
 import Category from "./components/category-container/Category-container.component";
 const App = () => {
   return (
