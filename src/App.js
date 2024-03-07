ecd265f84247244aba4a1e67be36fe92b044f0f7
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-07 18:14:50 +0200

    added sass

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 const App = () => {
   const categories = [
     {
+      id: 1,
       title: "Mens",
       ImageUrl: "/",
     },
     {
+      id: 2,
       title: "Womens",
       ImageUrl: "/",
     },
     {
+      id: 3,
+      title: "Jackets",
+      ImageUrl: "/",
+    },
+    {
+      id: 4,
       title: "Sneakers",
       ImageUrl: "/",
     },
     {
-      title: "Watches",
-      ImageUrl: "/",
-    },
-    {
-      title: "Jackets",
+      id: 5,
+      title: "Hats",
       ImageUrl: "/",
     },
   ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <img src="" alt="category-image" />
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map((category) => {
+        return (
+          <div key={category.id} className="category-container">
+            <img src={category.ImageUrl} alt="category-image" />
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
