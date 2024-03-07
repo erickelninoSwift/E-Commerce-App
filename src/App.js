6c499c50a8c3f66e07c96c757da595da3c217040
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-07 18:04:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 const App = () => {
+  const categories = [
+    {
+      title: "Mens",
+      ImageUrl: "/",
+    },
+    {
+      title: "Womens",
+      ImageUrl: "/",
+    },
+    {
+      title: "Sneakers",
+      ImageUrl: "/",
+    },
+    {
+      title: "Watches",
+      ImageUrl: "/",
+    },
+    {
+      title: "Jackets",
+      ImageUrl: "/",
+    },
+  ];
   return (
     <div className="categories-container">
       <div className="category-container">
         <img src="" alt="category-image" />
         <div className="category-body-container">
           <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src="" alt="category-image" />
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src="" alt="category-image" />
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src="" alt="category-image" />
-        <div className="category-body-container">
-          <h2>Watches</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src="" alt="category-image" />
-        <div className="category-body-container">
-          <h2>Jackets</h2>
           <p>Shop Now</p>
         </div>
       </div>
