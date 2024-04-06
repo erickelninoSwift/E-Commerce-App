c0f834988151eab39ec57538f777188380c4d44f
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-06 22:39:28 +0200

    added dasyUI to improve our ui

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -21,59 +21,64 @@ const SignUpForm = () => {
     console.log(user);
   };
   return (
-    <form onSubmit={(e) => handleLoginUser(e)}>
-      <div className="container" style={{ width: "30%" }}>
-        <label htmlFor="dname">
-          <b>Display Name</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="dname"
-          value={displayname}
-          onChange={(e) => setDisplayname(e.target.value)}
-          required
-        />
+    <>
+      <form onSubmit={(e) => handleLoginUser(e)}>
+        <div className="container" style={{ width: "30%" }}>
+          <label htmlFor="dname">
+            <b>Display Name</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="dname"
+            value={displayname}
+            onChange={(e) => setDisplayname(e.target.value)}
+            required
+          />
 
-        <label htmlFor="email">
-          <b>E-mail</b>
-        </label>
-        <input
-          type="email"
-          placeholder="Email Address"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+          <label htmlFor="email">
+            <b>E-mail</b>
+          </label>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-        <label htmlFor="psw">
-          <b>Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-          value={Password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="psw">
-          <b>Repeat Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Confirm password"
-          name="psw"
-          required
-          value={RePassword}
-          onChange={(e) => setRePassword(e.target.value)}
-        />
-        <button disabled={RePassword === Password ? false : true} type="submit">
-          Sign Up
-        </button>
-      </div>
-    </form>
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="psw">
+            <b>Repeat Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Confirm password"
+            name="psw"
+            required
+            value={RePassword}
+            onChange={(e) => setRePassword(e.target.value)}
+          />
+          <button
+            disabled={RePassword === Password ? false : true}
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,44 +1,149 @@
 import { Outlet, NavLink } from "react-router-dom";
 import "./navigation.styles.scss";
 // import logo from "../../assets/logo192.png";
-import logo from "../../assets/crown.svg";
 const Navigation = () => {
   return (
     <>
-      <div className="navigation">
-        <NavLink className="logo-container" to={"/"}>
-          <div>
-            <img
-              src={logo}
-              alt="logo"
-              className="logo"
-              style={{ objectFit: "fill" }}
-            />
+      <div className="navbar bg-base-300">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">J-SHOP</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink to={"/"}>HOME</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/shop"}>SHOP</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/signin"}>LOGIN</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-        </NavLink>
-
-        <div className="nav-links-container">
-          <NavLink className="nav-link" to={"/"} style={{ fontSize: "20px" }}>
-            HOME
-          </NavLink>
-          <NavLink
-            className="nav-link"
-            to={"/shop"}
-            style={{ fontSize: "20px" }}
-          >
-            SHOP
-          </NavLink>
-
-          <NavLink
-            className="nav-link"
-            to={"/signin"}
-            style={{ fontSize: "20px" }}
-          >
-            LOGIN
-          </NavLink>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
       <Outlet />
+      <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
+        <nav className="grid grid-flow-col gap-4">
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </div>
+        </nav>
+        <aside>
+          <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
     </>
   );
 };
