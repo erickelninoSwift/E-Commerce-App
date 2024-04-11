2e85060999c416345a94788a8e386317cf098304
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-11 16:51:26 +0200

    hotfix

--- src/components/Login/LoginFrom.jsx
+++ src/components/Login/LoginFrom.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import FromInput from "./FromInput";
 import { AuthChoice } from "./AuthChoice";
+import SignUpImage from "../signup/SignUpImage";
 const LoginFrom = ({ loginPage, islogin }) => {
   const [username, setUsername] = useState("");
   const [userPassword, setUserPassword] = useState("");
@@ -16,14 +17,7 @@ const LoginFrom = ({ loginPage, islogin }) => {
         <section className="h-screen w-[1048px]">
           <div className="container h-full px-6 py-24">
             <div className="flex h-full flex-wrap items-center justify-center lg:justify-between">
-              <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
-                <img
-                  src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                  className="w-full"
-                  alt="Phone image"
-                />
-              </div>
-
+              <SignUpImage />
               <div className="md:w-8/12 lg:ms-6 lg:w-5/12">
                 <h1 className="text-3xl mb-5">Login now !</h1>
                 <form onSubmit={(e) => handleLoginForm(e)}>
@@ -70,7 +64,7 @@ const LoginFrom = ({ loginPage, islogin }) => {
                         <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
                       </svg>
                     </span>
-                    Continue with Facebook
+                    Continue with Google
                   </a>
                 </form>
 
--- src/components/signup/Signupformv2.jsx
+++ src/components/signup/Signupformv2.jsx
@@ -7,7 +7,8 @@ import {
 } from "../../routes/signin/firebase";
 import FromInput from "../Login/FromInput";
 import { AuthChoice } from "../Login/AuthChoice";
-
+import GoogleButton from "../GoogleButton/GoogleButton";
+import SignUpImage from "./SignUpImage";
 const Signupformv2 = ({ loginUser, loginPage, islogin }) => {
   const [email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
@@ -52,14 +53,7 @@ const Signupformv2 = ({ loginUser, loginPage, islogin }) => {
           style={{ margin: "0px auto" }}
         >
           <div className="flex h-full flex-wrap items-center justify-center lg:justify-between">
-            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
-              <img
-                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                className="w-full"
-                alt="Phone image"
-              />
-            </div>
-
+            <SignUpImage />
             <div className="md:w-8/12 lg:ms-6 lg:w-5/12">
               <h2 className="text-3xl mb-5">Register now !</h2>
               <form onSubmit={(e) => handleSubmitButton(e)}>
@@ -101,45 +95,7 @@ const Signupformv2 = ({ loginUser, loginPage, islogin }) => {
                     OR
                   </p>
                 </div>
-
-                <div className="text-center">
-                  <button className="gsi-material-button" onClick={loginUser}>
-                    <div className="gsi-material-button-state"></div>
-                    <div className="gsi-material-button-content-wrapper">
-                      <div className="gsi-material-button-icon">
-                        <svg
-                          version="1.1"
-                          viewBox="0 0 48 48"
-                          style={{ display: "block" }}
-                        >
-                          <path
-                            fill="#EA4335"
-                            d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-                          ></path>
-                          <path
-                            fill="#4285F4"
-                            d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
-                          ></path>
-                          <path
-                            fill="#FBBC05"
-                            d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
-                          ></path>
-                          <path
-                            fill="#34A853"
-                            d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
-                          ></path>
-                          <path fill="none" d="M0 0h48v48H0z"></path>
-                        </svg>
-                      </div>
-                      <span className="gsi-material-button-contents">
-                        Continue with Google
-                      </span>
-                      <span style={{ display: "none" }}>
-                        Continue with Google
-                      </span>
-                    </div>
-                  </button>
-                </div>
+                <GoogleButton loginUserwithGoogle={loginUser} />
               </form>
               <AuthChoice
                 message={"You do have an account?"}
