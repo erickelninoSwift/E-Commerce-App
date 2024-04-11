8a04fb11cffc02a49fa3122c0772d09e8ff827ab
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-11 17:16:56 +0200

    hotfix

--- src/components/signup/Signupformv2.jsx
+++ src/components/signup/Signupformv2.jsx
@@ -90,7 +90,7 @@ const Signupformv2 = ({ loginUser, loginPage, islogin }) => {
                   Register
                 </button>
 
-                <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500">
+                {/* <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500">
                   <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
                     OR
                   </p>
@@ -98,10 +98,10 @@ const Signupformv2 = ({ loginUser, loginPage, islogin }) => {
                 <GoogleButton
                   loginUserwithGoogle={loginUser}
                   message={"REGISTER WITH GOOGLE"}
-                />
+                /> */}
               </form>
               <AuthChoice
-                message={"You do have an account?"}
+                message={"Already have an Account ?"}
                 message2={"Login"}
                 authMethod={loginPage}
                 method={islogin}
