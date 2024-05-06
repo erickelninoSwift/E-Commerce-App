169774573db75220483788c4bd08b119f31bba4e
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-06 02:40:24 +0200

    added redirects file

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Sponsors from "../../components/sponsor/Sponsors";
 import { useContext, useEffect } from "react";
 import { CartContext } from "../../contex/Cart.Context";
-import LoadSpinners from "../../components/Loaders/LoadSpinners";
+
 const Home = () => {
   const { setIsCartOpen } = useContext(CartContext);
   useEffect(() => {
