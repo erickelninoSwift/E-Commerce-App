49a0c1cc78ca6b9fce009013caeed0f274442ad0
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-24 18:06:22 +0200

    implementing useReducer and replacing useContext

--- src/contex/UserContext.jsx
+++ src/contex/UserContext.jsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useState,
-  useContext,
-  useEffect,
-  useReducer,
-} from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener } from "../routes/signin/firebase";
 
 export const UserContext = createContext({
@@ -15,6 +9,10 @@ export const UserContext = createContext({
 const STATE_TYPE = {
   SET_CURRNT_USER: "SET_CURRENT_USER",
 };
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
 const userReducer = (state, action) => {
   const { type, payload } = action;
 
@@ -24,18 +22,17 @@ const userReducer = (state, action) => {
         currentUser: payload,
       };
     default:
-      throw new Error(`The type prpvided ${type} does not exist `);
+      throw new Error(`The type provided ${type} does not exist `);
   }
 };
 
 export const UserProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(userReducer, {
-    currentUser: null,
-  });
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
   const { currentUser } = state;
-
+  console.log(currentUser);
+  console.log(dispatch);
   const setCurrentUser = (user) => {
-    dispatch({ type: STATE_TYPE, payload: user });
+    dispatch({ type: STATE_TYPE.SET_CURRNT_USER, payload: user });
   };
   const value = {
     currentUser,
@@ -45,9 +42,9 @@ export const UserProvider = ({ children }) => {
   useEffect(() => {
     try {
       const isUserLoggedIn = onAuthStateChangedListener((user) => {
-        setCurrentUser(() => user);
+        setCurrentUser(user);
       });
-      isUserLoggedIn();
+      return isUserLoggedIn;
     } catch (error) {
       console.log(`There was fetch failure ${error}`);
     }
